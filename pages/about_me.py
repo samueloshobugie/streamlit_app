53293755c2f38019f7a82df7398d1e55cd41dbc9
oshobugie <soshobugie@gmail.com>
2025-08-16 15:32:21 +0100

    more about me

--- pages/about_me.py
+++ pages/about_me.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
-st.title("About Me")
+@st.experimental_dialog("contact me")
+def show_contact_form():
+    st.text_input("first name")
+
+# --- hero section ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.image("./assets/runnaira_logo.png", width=230)
+with col2:
+    st.title("Samuel Oshougie", anchor=False)
+    st.write(
+        "Senior DevOps Engineer and Agentic AI Automation Expert."
+    )
+    if st.button("Contact Me"):
+        show_contact_form
+
+# --- experience ---
+st.write("\n")
+st.subheader("experience and qualification", anchor=False)
+st.write(
+    "7 years" \
+    "strong hands on" \
+    "excellent team player" 
+)
+
+st.write("\n")
+st.subheader("hard skills", anchor=False)
+st.write(
+    "python" \
+    "javascript" \
+    "devops" \
+    "ai agent automation" 
+)
